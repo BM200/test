20e4c490cdc9daced7a1a726903265fa0ab4bbb3
BM200 <32624320+BM200@users.noreply.github.com>
2019-02-22 18:07:02 -0600

    my fist commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*
+//vanilla javascript
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+/*
+input.addEventListener('keyup', function(){
+    h1.innerHTML = input.value;
+
+})
+input.addEventListener('keyup', ()=> h1.innerHTML = input.value);
+*/
+//VUE.js Crear instancia 
+new Vue({
+    el: 'main',
+
+    data:{
+        mensaje: 'Hola Mundo',
+        
+    }
+})
